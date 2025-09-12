37a0f3129436bf15073b0f108f907e68525715e4
Md Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2025-09-13 05:00:50 +0600

    resolve just company logo

--- src/layouts/partials/Banner.tsx
+++ src/layouts/partials/Banner.tsx
@@ -3,12 +3,14 @@ import Image from "next/image";
 import { BsStars } from "react-icons/bs";
 import React from "react";
 import { getMDData } from "@/helpers/mdHelper";
-import logoimg from "../../../public/images/Logo.png";
 import { markdownify } from "@/lib/utils/textConverter";
+import Link from "next/link";
 
 const Banner = () => {
   const homepage = getMDData<{ banner: any; aboutme: any }>("homepage/_index.md");
   const { banner } = homepage;
+  const logoPath = banner.logo || "/images/Logo.png";
+
   
   return (
     <section
@@ -18,17 +20,18 @@ const Banner = () => {
       <div data-aos="fade-up"
       data-aos-duration="1000" className="container relative">
         {/* Logo */}
-        <header className="text-center mb-8">
+       <header className="text-center mb-8">
+        <Link href="/">
           <Image
             className="mx-auto"
             height={27}
             width={120}
-            src={banner.logo || logoimg}
+            src={logoPath}
             alt="Company logo"
             priority
           />
-        </header>
-
+        </Link>
+      </header>
         {/* Banner Content */}
         <div className="row justify-center text-center mt-24">
           <div className="max-w-[960px] mb-8">
